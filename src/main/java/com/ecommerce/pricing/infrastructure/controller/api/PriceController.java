845f8115cfe0ab3a477d0d33f88cfd1772def204
walavo <alvarodaniel2808@gmail.com>
2025-05-27 22:24:55 -0500

    chore(openapi): change api contract and refactor method

--- src/main/java/com/ecommerce/pricing/infrastructure/controller/api/PriceController.java
+++ src/main/java/com/ecommerce/pricing/infrastructure/controller/api/PriceController.java
@@ -1,10 +1,12 @@
 package com.ecommerce.pricing.infrastructure.controller.api;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
-public class PriceController implements DefaultApi{
+public class PriceController implements PricingControllerApi{
 
 }
